7bc4b9aff665f77f4fb00ba121c0c277373b67bb
alana anderson <alanatreimanis@gmail.com>
2016-01-25 14:42:51 -0500

    browserify for js clientside

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,29 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var replace = require('gulp-html-replace');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sync = require('browser-sync');
 var webserver = require('gulp-webserver');
 var del = require('del');
 var rimraf = require('rimraf');
+var browserify = require('gulp-browserify');
 
 // Establish paths
 var paths = {
 	src: {
-		base: 'src'
+		base: 'src',
+		test: 'src/test' // will need for future tests
 	},
 	dest: {
-		base: 'build'
+		base: 'build',
+		test: 'build/test' // will need for future tests
 	},
 	page: {
 		name: 'index.html'
+	},
+	all: {
+		call: ['src/scripts/*.js', 'src/scripts/**/*.js', 'src/index.html'],
+
 	}
 }
 
@@ -40,10 +46,26 @@ gulp.task('clean', function (cb) {
 	rimraf(paths.dest.base, cb);
 });
 
+gulp.task('scripts', function() {
+	gulp.src('src/scripts/main.js', {read: false})
+		.pipe(browserify({
+		entries : ['./main.js'],
+		insertGlobals : true,
+		debug : !gulp.env.production
+		}))
+	.pipe(gulp.dest('build/js'))
+});
+
 gulp.task('less', ['clean'], function () {
 	return gulp.src('src/less/*.less')
 		.pipe(less())
 		.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('uglify', function () {
+	return gulp.src('build/js/*')
+		.pipe(uglify())
+		.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('html', function () {
@@ -54,21 +76,9 @@ gulp.task('html', function () {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('uglify', ['concat'], function () {
-	return gulp.src('build/js/*')
-		.pipe(uglify())
-		.pipe(gulp.dest('build/js'));
-});
-
-gulp.task('concat', function () {
-	return gulp.src(['src/scripts/utils.js', 'src/scripts/main.js'])
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('build/js'));
-});
-
 gulp.task('serve', ['default'], function(){
 	gulp.src(paths.dest.base)
 		.pipe(webserver(getServer()));
 });
 
-gulp.task('default', ['clean', 'less', 'concat', 'uglify', 'html']);
+gulp.task('default', ['clean', 'scripts', 'uglify', 'less', 'html']);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,1 @@
-console.log(utils.greeting);
+console.log('hey mic');
